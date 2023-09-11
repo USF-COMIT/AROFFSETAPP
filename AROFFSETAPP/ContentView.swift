fae1c747ca4da6a4d18a118d2ba5f16fea33d4b0
darshangummadi <98764752+darshangummadi@users.noreply.github.com>
2023-09-11 16:39:57 -0400

    Initial Commit

---
+++ AROFFSETAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AROFFSETAPP
+//
+//  Created by Darshan Gummadi on 9/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
